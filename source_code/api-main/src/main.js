5065d0fec8b19014f2693c7b7b3baa4f17699486
Milena Oliviera <imperior5@hotmail.com>
2025-05-31 19:06:09 -0300

    atualiza arquivo principal da aplicação

--- source_code/api-main/src/main.js
+++ source_code/api-main/src/main.js
@@ -1,14 +1,10 @@
-import { sequelize } from "./config/database.js";
 import express from "express";
 import "dotenv/config";
+import clientRoutes from "./routes/clientRoute.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection made successfully");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((e) => console.error("Error:", e));
+app.use(clientRoutes);
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));

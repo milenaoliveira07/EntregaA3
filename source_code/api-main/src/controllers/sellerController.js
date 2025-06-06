4ede883c7d8cc29e9a7f09ca8b73fbc3a452138b
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 16:13:57 -0300

    Adiciona validação da requisição no update e create

--- source_code/api-main/src/controllers/sellerController.js
+++ source_code/api-main/src/controllers/sellerController.js
@@ -1,56 +1,66 @@
- import { SellerService } from "../services/sellerService.js";
+import { createSeller, updateSeller } from "../schemas/sellerSchema.js";
+import { SellerService } from "../services/sellerService.js";
 
- const sellerService = new SellerService();
- class SellerController {
-    async findAll(req, res) {
-        try {
-            const sellers = await sellerService.findAll();    
-            return res.status(200).json({ sellers: sellers });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+const sellerService = new SellerService();
+class SellerController {
+  async findAll(req, res) {
+    try {
+      const sellers = await sellerService.findAll();
+      return res.status(200).json(sellers);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    async findById(req, res) {
-        try {
-            const id = req.params.id;
-            const seller = await sellerService.findById(id);
-            res.status(200).json({ seller: seller });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  async findById(req, res) {
+    try {
+      const id = validateID(req.params.id);
+
+      const seller = await sellerService.findById(id);
+      res.status(200).json(seller);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    async create(req, res) {
-        try {
-            const data = req.body;
-            const seller = await sellerService.create(data);
-            res.status(200).json({ seller: seller });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  async create(req, res) {
+    try {
+      const { error, value } = createSeller.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      const seller = await sellerService.create(value);
+      res.status(200).json(seller);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    async update(req, res) {
-        try {
-            const id = req.params.id;
-            const data = req.body;
-            const seller = await sellerService.update(id, data);
-            res.status(200).json({ seller: seller });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  async update(req, res) {
+    try {
+      const id = validateID(req.params.id);
+
+      const { error, value } = updateSeller.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      const seller = await sellerService.update(id, value);
+      res.status(200).json(seller);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    async delete(req, res) {
-        try {
-            const id = req.params.id;
-            const seller = await sellerService.delete(id);
-            res.status(200).json({ message: "Seller deleted successfully" });
-        } catch (error) {
-            res.status(500).json(error.message);
-        }
+  async delete(req, res) {
+    try {
+      const id = validateID(req.params.id);
+
+      await sellerService.delete(id);
+      res.status(200).json({ message: "Seller deleted successfully" });
+    } catch (error) {
+      res.status(500).json(error.message);
     }
- }
+  }
+}
 
- export default new SellerController();
+export default new SellerController();

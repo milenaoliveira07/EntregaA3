6ded510a64c229c0f446a7c265e51097bb7307a9
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 16:16:38 -0300

    Atualiza o schema de client adicionando validação do número

--- source_code/api-main/src/schemas/clientSchema.js
+++ source_code/api-main/src/schemas/clientSchema.js
@@ -3,7 +3,11 @@ import joi from "joi";
 export const createClient = joi.object({
   full_name: joi.string().max(80).required(),
   email: joi.string().email().max(150).required(),
-  phone: joi.string().max(15).required(),
+  phone: joi
+    .string()
+    .pattern(/^([1-9]{2})9\d{8}$/)
+    .required(),
+
   address: joi.string().max(80),
 });
 

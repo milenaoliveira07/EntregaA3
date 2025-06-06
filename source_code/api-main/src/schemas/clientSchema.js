b0db8aaad6f005a4bbc1bda1b51f465240070490
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 15:36:49 -0300

    Adiciona schema de validação para client

--- source_code/api-main/src/schemas/clientSchema.js
+++ source_code/api-main/src/schemas/clientSchema.js
@@ -1,10 +1,15 @@
 import joi from "joi";
 
 export const createClient = joi.object({
-  full_name: joi.string().required(),
-  email: joi.string().email().required(),
-  phone: joi.string().required()
+  full_name: joi.string().max(80).required(),
+  email: joi.string().email().max(150).required(),
+  phone: joi.string().max(15).required(),
+  address: joi.string().max(80),
 });
 
-
-export const updateClient = joi.object({});
+export const updateClient = joi.object({
+  full_name: joi.string().max(80),
+  email: joi.string().email().max(150),
+  phone: joi.string().max(15),
+  address: joi.string().max(80),
+});

5a98af2fdcffa57d3db14bc45ed2d9a5bdd875ae
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 15:24:10 -0300

    Atualiza o arquivo principal da aplicação

--- source_code/api-main/src/main.js
+++ source_code/api-main/src/main.js
@@ -1,13 +1,13 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
 import clientRoutes from "./routes/clientRoute.js";
 import sellerRoutes from "./routes/sellerRoute.js";
+import productRoutes from "./routes/productRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api", clientRoutes);
-app.use("/api", sellerRoutes);
+app.use("/api", clientRoutes, sellerRoutes, productRoutes);
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));

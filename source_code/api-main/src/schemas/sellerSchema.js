fca5135b84778fb5b5f19e75bc5536c3683433c8
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 15:36:19 -0300

    Adiciona schema de validação para seller

--- source_code/api-main/src/schemas/sellerSchema.js
+++ source_code/api-main/src/schemas/sellerSchema.js
@@ -1,9 +1,12 @@
 import joi from "joi";
 
 export const createSeller = joi.object({
-  full_name: joi.string().required(),
-  email: joi.string().email().required(),
-  registration: joi.string().required()
+  full_name: joi.string().max(80).required(),
+  email: joi.string().email().max(150).required(),
+  registration_prefix: joi.string().max(10).required(),
 });
 
-export const updateSeller = joi.object({});
+export const updateSeller = joi.object({
+  full_name: joi.string().max(80),
+  email: joi.string().email().max(150),
+});

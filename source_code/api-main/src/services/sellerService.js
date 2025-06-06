bc87c0f8bebe6da1f104235169017d20b4e05639
Milena Oliviera <imperior5@hotmail.com>
2025-06-06 16:16:58 -0300

    Atualiza service de seller

--- source_code/api-main/src/services/sellerService.js
+++ source_code/api-main/src/services/sellerService.js
@@ -2,43 +2,47 @@ import db from "../config/database.js";
 import { SellerRepository } from "../repositories/sellerRepository.js";
 
 export class SellerService {
-    constructor() {
-        this.sellerRepository = new SellerRepository(db);
-    }
+  constructor() {
+    this.sellerRepository = new SellerRepository(db);
+  }
 
-    async findAll() {
-        return await this.sellerRepository.findAll();
-    }
+  async findAll() {
+    return await this.sellerRepository.findAll();
+  }
 
-    async findById(id) {
-        const seller = await this.sellerRepository.findById(id);
-        if (!seller) {
-            throw new Error("Seller not found");
-        }
-        return seller;
+  async findById(id) {
+    const seller = await this.sellerRepository.findById(id);
+    if (!seller) {
+      throw new Error("Seller not found");
     }
+    return seller;
+  }
 
-    async create(data) {
-        const { full_name, email, registration } = data;
-        if (!full_name || !email || !registration) {
-            throw new Error("Full name, email and registration fields are required");
-        }
-        return await this.sellerRepository.create(data);
-    }
+  async create(data) {
+    return await this.sellerRepository.create(data);
+  }
 
-    async update(id, data) {
-        const seller = await this.findById(id);
-        if (!seller) {
-            throw new Error("Seller not found");
-        }
-        return await this.sellerRepository.update(id, data);
+  async update(id, data) {
+    const seller = await this.findById(id);
+    if (!seller) {
+      throw new Error("Seller not found");
     }
+    return await this.sellerRepository.update(id, data);
+  }
 
-    async delete(id) {
-        const seller = await this.findById(id);
-        if (!seller) {
-            throw new Error("Seller not found");
-        }
-        return await this.sellerRepository.delete(id);
+  async delete(id) {
+    const seller = await this.findById(id);
+    if (!seller) {
+      throw new Error("Seller not found");
     }
+    return await this.sellerRepository.delete(id);
+  }
+
+  async findByOne(field, value) {
+    const seller = await this.sellerRepository.findByOne(field, value);
+    if (!seller) {
+      throw new Error("Seller not found");
+    }
+    return seller;
+  }
 }

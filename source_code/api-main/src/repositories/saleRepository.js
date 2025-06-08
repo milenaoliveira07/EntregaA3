2ebdac292038f2c2c0739e4868e0b3e076335910
Milena Oliviera <imperior5@hotmail.com>
2025-06-08 14:36:12 -0300

    Adiciona repository para sale

--- source_code/api-main/src/repositories/saleRepository.js
+++ source_code/api-main/src/repositories/saleRepository.js
@@ -1,12 +1,6 @@
 import { GenericRepository } from "./genericRepository.js";
-import { dbConnection } from "../config/database.js";
-
 export class SaleRepository extends GenericRepository {
-  constructor() {
-    super(dbConnection, "sales");
-  }
-
-  async findByCustomerId(customerId) {
-    return this.findByOne("customer_id", customerId);
+  constructor(connection) {
+    super(connection, "sales");
   }
 }

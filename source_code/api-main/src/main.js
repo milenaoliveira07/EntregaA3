be629bb481f2380bc1bdd8b5265929f6496f9500
Milena Oliviera <imperior5@hotmail.com>
2025-06-08 14:38:08 -0300

    Adiciona rota de sales na API

--- source_code/api-main/src/main.js
+++ source_code/api-main/src/main.js
@@ -3,11 +3,12 @@ import express from "express";
 import clientRoutes from "./routes/clientRoute.js";
 import sellerRoutes from "./routes/sellerRoute.js";
 import productRoutes from "./routes/productRoute.js";
+import saleRoutes from "./routes/saleRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api", clientRoutes, sellerRoutes, productRoutes);
+app.use("/api", clientRoutes, sellerRoutes, productRoutes, saleRoutes);
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
